032403844f810a93f8add2d9fe25d8a0660bda2f
Robert Pearce <me@robertwpearce.com>
2015-10-05 23:35:08 -0400

    Refactors fs promises in to their own fsUtils file

--- src/index.js
+++ src/index.js
@@ -1,50 +1,24 @@
-import fs from 'fs';
 import path from 'path';
 import handlebars from 'handlebars';
 import marked from 'marked';
+import { mkDir, readDir, readFile, writeFile } from './fsUtils';
 
 const buildPath = '_site';
 const postsPath = path.join(__dirname, 'posts');
 const layoutPath = path.join(__dirname, 'layout.html');
 
-async function mkDir(path) {
-  return new Promise(function(resolve, reject) {
-    fs.mkdir(path, function(err, data) {
-      if (err) reject(err);
-      resolve(data);
-    });
-  });
-}
-
-async function readDir(path) {
-  return new Promise(function(resolve, reject) {
-    fs.readdir(path, function(err, data) {
-      if (err) reject(err);
-      resolve(data);
-    });
-  });
-}
-
-async function readFile(path) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(path, 'utf8', function(err, data) {
-      if (err) reject(err);
-      resolve(data);
-    });
-  });
-}
-
-async function writeFile(path, content) {
-  return new Promise(function(resolve, reject) {
-    fs.writeFile(path, content, 'utf8', function(err, data) {
-      if (err) reject(err);
-      resolve(data);
-    });
-  });
-}
-
 function filterMdFiles(files) {
   return files.filter(file => { return file.match(/.md|.markdown/); });
+}
+
+async function outputMdFileAsHTML(mdFilename) {
+  var mdFilePath = path.join(postsPath, mdFilename);
+  var mdData = await readFile(mdFilePath);
+  var mdHTML = marked(mdData);
+  var layout = await readFile(layoutPath);
+  var template = handlebars.compile(layout);
+  var html = template({ markup: mdHTML });
+  await writeFile(`${buildPath}/${mdFilename.split('.')[0]}.html`, html);
 }
 
 async () => {
@@ -53,60 +27,9 @@ async () => {
     const files = await readDir(postsPath);
     const mdFilenames = filterMdFiles(files);
     for (let mdFilename of mdFilenames) {
-      let mdFilePath = path.join(postsPath, mdFilename);
-      let mdData = await readFile(mdFilePath);
-      let mdHTML = marked(mdData);
-
-      let layout = await readFile(layoutPath);
-      let template = handlebars.compile(layout);
-      let html = template({ markup: mdHTML });
-      await writeFile(`${buildPath}/${mdFilename.split('.')[0]}.html`, html);
+      await outputMdFileAsHTML(mdFilename);
     }
   } catch (err) {
     console.error(err);
   }
 }()
-
-
-//fs.readdir(postsPath, function(err, files) {
-//  if (err) throw err;
-
-//  files
-//    .filter(function(file) { return file.match(/.md|.markdown/); })
-//    .forEach(function(name) {
-//      const filePath = path.join(postsPath, name);
-
-//      fs.readFile(filePath, 'utf8', function(err, data) {
-//        if (err) throw err;
-//        const markdownHTML = marked(data);
-
-//        fs.readFile(layoutPath, 'utf8', function(err, data) {
-//          const template = handlebars.compile(data);
-
-//          console.log(template({ markup: markdownHTML }));
-//        });
-//      });
-//    });
-//});
-
-
-//fs.readdir(postsPath, function(err, files) {
-//  if (err) throw err;
-
-//  files
-//    .filter(function(file) { return file.match(/.md|.markdown/); })
-//    .forEach(function(name) {
-//      const filePath = path.join(postsPath, name);
-
-//      fs.readFile(filePath, 'utf8', function(err, data) {
-//        if (err) throw err;
-//        const markdownHTML = marked(data);
-
-//        fs.readFile(layoutPath, 'utf8', function(err, data) {
-//          const template = handlebars.compile(data);
-
-//          console.log(template({ markup: markdownHTML }));
-//        });
-//      });
-//    });
-//});
