9da590ef29f12b69a3f0c9eb0f3f65a0886dce0e
Robert Pearce <me@robertwpearce.com>
2015-10-09 21:14:24 -0400

    Splitting metadata from content and doing so from a helper file

--- src/index.js
+++ src/index.js
@@ -1,66 +1,8 @@
-import path from 'path';
-import handlebars from 'handlebars';
-import marked from 'marked';
-import fsp from 'fs-promise';
-import yaml from 'js-yaml';
-
-const buildPath = '_site';
-const postsPath = path.join(__dirname, 'posts');
-const layoutPath = path.join(__dirname, 'layouts', 'post.html');
-
-async function getMdFilenames() {
-  const files = await fsp.readdir(postsPath);
-  return files.filter(file => { return file.match(/.md|.markdown/); });
-}
-
-async function outputMdFileAsHTML(mdFilename) {
-  // get file data
-  const mdFilePath = path.join(postsPath, mdFilename);
-  const mdData = await fsp.readFile(mdFilePath, 'utf8');
-
-  // separate metadata from content
-  var metadata;
-  var lines = mdData.split('\n').map(line => { return line.trim(); });
-
-  // find `---` indices
-  var indices = [];
-  lines.forEach(function(line, i) {
-    if (/^---/.test(line)) {
-      indices.push(i);
-    }
-  });
-
-  // parse out actual metadata and convert to JSON
-  metadata = lines.splice(indices[0], indices[1] + 1);
-  metadata.shift();
-  metadata.pop();
-  metadata = yaml.safeLoad(metadata.join('\n'));
-
-  // convert markdown to html
-  const mdHTML = marked(lines.join('\n'));
-
-  // get layout file
-  const layout = await fsp.readFile(layoutPath, 'utf8');
-
-  // build up layout and compile template
-  const template = handlebars.compile(layout);
-  const html = template({
-    title: metadata.title,
-    description: metadata.description,
-    markup: mdHTML
-  });
-
-  // write file to build path
-  await fsp.writeFile(`${buildPath}/${mdFilename.split('.')[0]}.html`, html, 'utf8');
-}
+import mdHelper from './mdHelper';
 
 async () => {
   try {
-    await fsp.mkdir(buildPath);
-    const mdFilenames = await getMdFilenames();
-    for (let mdFilename of mdFilenames) {
-      await outputMdFileAsHTML(mdFilename);
-    }
+    mdHelper.convertMds();
   } catch (err) {
     console.error(err);
   }
