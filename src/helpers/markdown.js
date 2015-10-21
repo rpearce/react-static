e11a0e3700a3e21c1bfa28fde1b6803ae4f0f15d
Robert Pearce <me@robertwpearce.com>
2015-10-21 11:15:05 -0400

    Metadata/content refactor. Still not happy

--- src/helpers/markdown.js
+++ src/helpers/markdown.js
@@ -2,33 +2,36 @@ import marked from 'marked';
 import yaml from 'js-yaml';
 
 function splitMetadataFromContent(contents) {
-  var fileAttrs = {};
-  var lines = contents.split('\n').map(line => { return line.trim(); });
-  var yamlIndices = getMetadataBoundaryIndices(lines);
-
-  if (yamlIndices.length > 0) {
-    let tempMetadata = lines.splice(yamlIndices[0], yamlIndices[1] + 1);
-    tempMetadata.shift();
-    tempMetadata.pop();
-
-    fileAttrs.metadata = yaml.safeLoad(tempMetadata.join('\n'));
-  }
-
-  fileAttrs.content = marked(lines.join('\n'));
-
-  return fileAttrs;
+  const lines = contents.split('\n').map(line => { return line.trim(); });
+  const metadataIndices = getMetadataBoundaryIndices(lines);
+  const metadata = getMetadata({ lines, metadataIndices });
+  const content = getContent({ lines, metadataIndices });
+  return { metadata, content };
 }
 
 function getMetadataBoundaryIndices(lines) {
-  return lines.map(testForBoundary).clean(undefined);
+  return lines.map(testForMetadataBoundary).clean(undefined);
 }
 
-function testForBoundary(item, i) {
+function testForMetadataBoundary(item, i) {
   if (/^---/.test(item)) {
     return i;
   }
 }
 
-export default {
-  splitMetadataFromContent
-};
+function getMetadata({ lines, metadataIndices }) {
+  if (metadataIndices.length > 0) {
+    let tempMetadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1]);
+    return yaml.safeLoad(tempMetadata.join('\n'));
+  }
+  return {};
+}
+
+function getContent({ lines, metadataIndices }) {
+  if (metadataIndices.length > 0) {
+    lines = lines.slice(metadataIndices[1] + 1, lines.length);
+  }
+  return marked(lines.join('\n'));
+}
+
+export default { splitMetadataFromContent };
